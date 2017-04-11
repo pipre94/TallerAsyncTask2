7521c42cc87593812d80e7d827869ea4c2338b27
Jonathan Mideros <10jonmid@gmail.com>
2017-04-11 12:43:56 -0500

    Proyecto para realizar en semana santa

---
+++ app/src/main/java/com/jonmid/tallerasynctask/HttpManager.java
@@ -0,0 +1,28 @@
+package com.jonmid.tallerasynctask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpManager {
+
+    public static String getData(String uri) throws IOException {
+        BufferedReader reader = null;
+        URL url = new URL(uri);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String line;
+
+        while ( (line = reader.readLine()) != null ){
+            stringBuilder.append(line+"\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
---
+++ app/src/main/java/com/jonmid/tallerasynctask/MainActivity.java
@@ -0,0 +1,88 @@
+package com.jonmid.tallerasynctask;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar cargador;
+    Button boton;
+    TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cargador = (ProgressBar) findViewById(R.id.cargador);
+        boton = (Button) findViewById(R.id.boton);
+        texto = (TextView) findViewById(R.id.texto);
+    }
+
+    public Boolean isOnLine(){
+        ConnectivityManager connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connec.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void onClickButton(View view){
+        if (isOnLine()){
+            MyTask task = new MyTask();
+            task.execute("https://jsonplaceholder.typicode.com/posts");
+        }else {
+            Toast.makeText(this, "Sin conexión", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cargarDatos(){
+        texto.append("JSON cargado correctamente.");
+    }
+
+    private class MyTask extends AsyncTask<String, String, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            cargador.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String content = null;
+            try {
+                content = HttpManager.getData(params[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            cargarDatos();
+            cargador.setVisibility(View.GONE);
+        }
+    }
+}
